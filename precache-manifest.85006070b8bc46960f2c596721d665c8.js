fb85b33fa93cfa5fd0ada5951925f5ffffc75200
vslemon <vsl.lemon@gmail.com>
2021-05-30 00:52:25 +0300

    Updates

---
+++ precache-manifest.85006070b8bc46960f2c596721d665c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0028c39e5002d553ff5cac5e3cb7a0fc",
+    "url": "/vslemon/auction-app/index.html"
+  },
+  {
+    "revision": "4b90092ce3ec204e6c17",
+    "url": "/vslemon/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a68febd506dd8cbba523",
+    "url": "/vslemon/auction-app/static/js/2.7a7e1c0a.chunk.js"
+  },
+  {
+    "revision": "4b90092ce3ec204e6c17",
+    "url": "/vslemon/auction-app/static/js/main.cf71a757.chunk.js"
+  },
+  {
+    "revision": "4847f153baab7732b352",
+    "url": "/vslemon/auction-app/static/js/runtime-main.e1aab83e.js"
+  }
+]);
